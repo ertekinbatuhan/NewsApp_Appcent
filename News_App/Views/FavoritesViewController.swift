42d7083d742a5aa60269144bb6a7b30093c528a2
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-05-08 20:35:32 +0300

    upload project

---
+++ News_App/Views/FavoritesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesViewController.swift
+//  News_App
+//
+//  Created by Batuhan Berk Ertekin on 8.05.2024.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        
+    }
+    
+}
---
+++ News_App/Views/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News_App
+//
+//  Created by Batuhan Berk Ertekin on 8.05.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
